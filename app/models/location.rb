ace77aaaaf0ad992451bebe9a1c32e03ddd8f722
algoregalore <cemurray88@gmail.com>
2017-06-29 08:48:05 -0500

    add cities to drop down

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,6 +3,32 @@ class Location < ApplicationRecord
   geocoded_by :full_street_address
   after_validation :geocode
 
+  CITY_LIST = ['Atlanta',
+              'Alpharetta',
+              'Avondale Estates',
+              'Chamblee',
+              'Clarkston',
+              'College Park',
+              'Decatur',
+              'Doraville',
+              'Dunwoody',
+              'East Point',
+              'Ellenwood',
+              'Fairburn',
+              'Forest Park',
+              'Hapeville',
+              'Jonesboro',
+              'Lake City',
+              'Lovejoy',
+              'Lithonia',
+              'Morrow',
+              'Palmetto',
+              'Riverdale',
+              'Roswell',
+              'Sandy Springs',
+              'Stone Mountain',
+              'Union City']
+
   def full_street_address
     "#{street_address}, #{city}, GA"
   end
